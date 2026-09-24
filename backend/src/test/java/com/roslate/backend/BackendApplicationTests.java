0f275dd07b6a896122d530beca730e27fe60d0de
roSlate <rodrigoserradiogo@gmail.com>
2026-09-24 13:36:09 +0100

    Added Spring Boot backend scaffolding

---
+++ backend/src/test/java/com/roslate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roslate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
