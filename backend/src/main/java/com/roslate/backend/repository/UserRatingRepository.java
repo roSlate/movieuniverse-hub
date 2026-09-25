4bd945e54b61038658c712805c5d77007283e815
roSlate <rodrigoserradiogo@gmail.com>
2026-09-25 15:22:22 +0100

    Add query method for a movie's ratings

--- backend/src/main/java/com/roslate/backend/repository/UserRatingRepository.java
+++ backend/src/main/java/com/roslate/backend/repository/UserRatingRepository.java
@@ -4,8 +4,11 @@ import com.roslate.backend.model.AppUser;
 import com.roslate.backend.model.UserRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRatingRepository extends JpaRepository<UserRating, Long> {
     Optional<UserRating> findByUserAndTmdbId(AppUser user, Long tmdbId);
+
+    List<UserRating> findByTmdbId(Long tmdbId);
 }
